b2ce5f22471b17b8be1cc22ff43093b3ab03a466
M4r10R0j4S <149609436+M4r10R0j4S@users.noreply.github.com>
2026-07-17 19:19:16 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+
+    page_title="Diabetes en México",
+
+    page_icon="🩺",
+
+    layout="wide"
+
+)
+
+st.title("🩺 Diabetes en México")
+
+st.write("Dashboard epidemiológico")
